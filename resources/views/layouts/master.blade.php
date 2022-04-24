c08523ee3d1284c4572b6b547869b39810b0c8cd
abi30 <rakib0751@gmail.com>
2022-04-24 05:02:18 +0200

    initial setup

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+
+<head>
+
+    <!-- Google Font: Source Sans Pro -->
+    @include('layouts.partials._head')
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+        @include('layouts.partials._navbar')
+        <!-- Navbar -->
+
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        @include('layouts/partials/_sidebar')
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            @yield('content')
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Control Sidebar -->
+        @include('layouts/partials/_sidebar_bottom')
+
+        <!-- /.control-sidebar -->
+
+        <!-- Main Footer -->
+        @include('layouts/partials/_footer')
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+
+    <!-- jQuery -->
+
+
+    @include('layouts/partials/_footer_script')
+
+</body>
+
+</html>
---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; @php
+          echo date('Y');
+    @endphp <a href="http://alrakib.codefactory.live/">abrakib.codefactory.live</a>.</strong> All rights reserved.
+  </footer>
